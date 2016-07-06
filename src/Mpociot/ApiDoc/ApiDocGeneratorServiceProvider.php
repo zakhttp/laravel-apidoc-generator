16c60a4722fc062682a33c831f329b6ec2d7dbd4
zakhttp <zakhttp@gmail.com>
2016-07-06 23:32:02 +0400

    Fix docblock

--- src/Mpociot/ApiDoc/ApiDocGeneratorServiceProvider.php
+++ src/Mpociot/ApiDoc/ApiDocGeneratorServiceProvider.php
@@ -44,7 +44,7 @@ class ApiDocGeneratorServiceProvider extends ServiceProvider
     }
 
     /**
-     * Return a fully qualified path to a given file relative to the storage directory
+     * Return a fully qualified path to a given file
      *
      * @param string $path
      * @return string
